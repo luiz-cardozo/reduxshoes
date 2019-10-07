48d0ed8fc4b5d2a0ae8abbd4cdb5238dc97186f3
Luiz Cardozo <luizfcardozo@gmail.com>
2019-10-07 07:22:13 -0300

    pull data from api and format price

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
+import React, { Component } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+import { formatPrice } from "../../util/format";
+import api from "../../services/api";
+import { ProductList } from "./styles";
 
-import { ProductList } from './styles';
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/28/COL-3586-128/COL-3586-128_detalhe1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$199,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+  async componentDidMount() {
+    const response = await api.get("products");
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />3
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
